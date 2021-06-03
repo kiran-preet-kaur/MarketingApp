2ade25f179c43ef114cc32964b5c582572a5356d
Kiranpreet Kaur <kiran.kalra487@gmail.com>
2021-06-03 15:26:03 +0530

    deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -17,6 +18,16 @@ app.use(express.json({ extended: false }))
 app.use('/api/users', require('./routes/users'));
 app.use('/api/login', require('./routes/login'));
 app.use('/api/mail', require('./routes/mail'));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
